637451cf7821f196b3489162fe09ae556fcec90b
Axceria <145180328+axceria@users.noreply.github.com>
2024-03-24 07:53:09 +0800

    Update random_string.py

--- lib/random_string.py
+++ lib/random_string.py
@@ -5,7 +5,13 @@ import string
 
 
 # Generate a random string based on a given length
+
 def generate_random_string(length):
     characters = string.ascii_letters + string.digits
-    random_string = ''.join(random.choice(characters) for _ in range(length))
+    random_string = ''.join(random.choice(characters) for _ in range(length - 2))  # Subtract 2 for start and end letters
+    random_string = random.choice(string.ascii_letters) + random_string + random.choice(string.ascii_letters)  # Ensure start and end with a letter
+    random_string_list = list(random_string)
+    for _ in range(5):  # Shuffle 5 times
+        random.shuffle(random_string_list)
+    random_string = ''.join(random_string_list)
     return random_string
